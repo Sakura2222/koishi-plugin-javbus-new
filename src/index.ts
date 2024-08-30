e861db570f25c6a504fbbc3f3ad5e034748ccecc
Lv RongTao <49859562+Sakura2222@users.noreply.github.com>
2024-08-30 14:59:06 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { Context, segment } from 'koishi'
-import Schema from 'schemastery'
+import { Context, segment } from "koishi";
+import Schema from "schemastery";
 
-
-export const name = 'javbus'
+export const name = "javbus";
 
 export interface Config {
   apiPrefix: string;
@@ -11,59 +10,89 @@ export interface Config {
 }
 
 export const Config = Schema.object({
-  description: Schema.string().default('这里不用填写东西').description('该插件请低调使用, 请勿配置于QQ或者是其他国内APP平台, 带来的后果请自行承担'),
-  apiPrefix: Schema.string().default('').required().description('请填写你的api地址前缀'),
-  allowDownloadLink: Schema.boolean().default(false).description('是否允许返回磁力链接'),
-  allowPreviewCover: Schema.boolean().default(false).description('是否允许返回封面预览(包含NSFW内容, 请酌情选择需不需要开启该配置项)')
-})
+  description: Schema.string()
+    .default("这里不用填写东西")
+    .description(
+      "该插件请低调使用, 请勿配置于QQ或者是其他国内APP平台, 带来的后果请自行承担"
+    ),
+  apiPrefix: Schema.string()
+    .default("")
+    .required()
+    .description("请填写你的api地址前缀（https://www.xxx.com/）"),
+  allowDownloadLink: Schema.boolean()
+    .default(false)
+    .description("是否允许返回磁力链接"),
+  allowPreviewCover: Schema.boolean()
+    .default(false)
+    .description("是否允许返回封面预览(弃用，不返回)"),
+});
 
-export const movieDetailApi = '/api/v1/movies/';
-export const starDetailApi = '/api/v1/stars/';
+export const movieDetailApi = "/movies/";
+
+export const magnetsDetailApi = "/magnets/";
 
 export function apply(ctx: Context, config: Config) {
-  async function fetchMovieDetail(id: string) {
-    return await ctx.http.get(config.apiPrefix + movieDetailApi  + id);
-  };
+  interface MovieDetails {
+    title?: string;
+    id?: number;
+    gid?: number;
+    date?: string;
+    uc?: string;
+    stars?: Array<{ name: string }>;
+    magnets: string;
+  }
+  async function fetchMovieDetails(number: string): Promise<MovieDetails> {
+    const movieUrl = config.apiPrefix + movieDetailApi + number;
+    // console.log("Fetching details from:", movieUrl);
+    let result = {
+      magnets: "",
+    };
+    // 第一个请求
+    const movieData = await ctx.http.get(movieUrl);
+    const { title, id, gid, date, uc, stars } = movieData;
+    result = { ...result, ...{ title, id, gid, date, uc, stars } };
 
-  async function fetchStarDetail(id: string) {
-    return await ctx.http.get(config.apiPrefix + starDetailApi  + id);
-  };
+    if (config.allowDownloadLink) {
+      // 使用第一个请求的返回数据来构造第二个请求的参数
+      // https://jav.hina.asia/api/magnets/JUQ-434?gid=56326057355&uc=0
+      const magnetsUrl =
+        config.apiPrefix + magnetsDetailApi + `${id}?gid=${gid}&uc=${uc}`;
+      //   console.log("Fetching details from:", magnetsUrl);
 
-  ctx.command('jav <id:text>', '查找javbus番号')
-    .action(async ({session}, id) => {
-      try {
-        if (!id) return '请提供番号!'
-        const result = await fetchMovieDetail(id)
-        const {
-          title,
-          img,
-          magnets,
-          date,
-          publisher,
-          stars
-        } = result;
-        const coverImg = await ctx.http.get<ArrayBuffer>(img, {
-          responseType: 'arraybuffer',
-        });
-        const publisherName = publisher?.name;
-        const starsArray = stars.map(star => star.name);
-        const starString = starsArray.length > 1 ? starsArray.join(', ') : starsArray[0];
-        const magnetsWithSubtitle = magnets.filter(item => item.hasSubtitle);
-        const magnetsMaxBytes = magnetsWithSubtitle && magnetsWithSubtitle.length ? magnetsWithSubtitle.find(m => Math.max(m.numberSize)) : magnets.find(m => Math.max(m.numberSize));
+      // 第二个请求
+      const magnetsList = await ctx.http.get(magnetsUrl);
+      //   console.log("magnetsList:", magnetsList);
 
-        if (!config.allowDownloadLink && !config.allowPreviewCover) {
-          await session.sendQueued(`标题: ${title}\n发行日期: ${date}\n女优: ${starString}\n发行商: ${publisherName}`)
-        } else if (config.allowDownloadLink && !config.allowPreviewCover) {
-          await session.sendQueued(`标题: ${title}\n发行日期: ${date}\n女优: ${starString}\n发行商: ${publisherName}\n磁力: ${magnetsMaxBytes.link}`)
-        } else if (!config.allowDownloadLink && config.allowPreviewCover) {
-          await session.sendQueued(`标题: ${title}\n发行日期: ${date}\n女优: ${starString}\n发行商: ${publisherName}\n${segment.image(coverImg)}`)
-        } else {
-          console.log('123')
-          await session.sendQueued(`标题: ${title}\n发行日期: ${date}\n女优: ${starString}\n发行商: ${publisherName}\n磁力: ${magnetsMaxBytes.link}${segment.image(coverImg)}`)
+      if (magnetsList.length > 0) {
+        result.magnets = `磁力: ${magnetsList[0].size},${magnetsList[0].shareDate}\n${magnetsList[0].link}`;
+
+        if (magnetsList.length > 1) {
+          result.magnets += `\n磁力2: ${
+            magnetsList[magnetsList.length - 1].size
+          },${magnetsList[magnetsList.length - 1].shareDate}\n${
+            magnetsList[magnetsList.length - 1].link
+          }`;
         }
-      } catch(err) {
+      }
+    }
+    return result;
+  }
+
+  ctx
+    .command("jav <number:text>", "查找javbus番号")
+    .action(async ({ session }, number) => {
+      try {
+        if (!number) return "请提供番号!";
+        const result = await fetchMovieDetails(number);
+        // console.log("result:", result);
+        const { title, id, gid, date, uc, stars, magnets } = result;
+        let str = `标题: ${title}\n发行日期: ${date}\n女优: ${stars
+          .map((star) => star.name)
+          .join(",")}\n${magnets}`;
+        await session.sendQueued(` ${str}`);
+      } catch (err) {
         console.log(err);
-        return `发生错误!请检查发送的番号是否正确;  ${err}`;
+        return `发生错误!请检查发送的番号是否正确(必须带横杠xxx-123);  ${err}`;
       }
     });
 }
